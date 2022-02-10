e17536e6462ee542f77aea4155f5d8fa898e44b9
Rahul Kumar Gupta <rahulguptasonu123@gmail.com>
2022-02-10 10:04:02 +0530

    wa

--- lib/ncertvideos/videosncert/playvideo.dart
+++ lib/ncertvideos/videosncert/playvideo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ncertclass1to12th/ncertvideos/videosncert/landscopeplayvideo.dart';
 import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 class PlayVideos extends StatefulWidget {
@@ -20,21 +21,40 @@ class _PlayVideosState extends State<PlayVideos> {
       params: YoutubePlayerParams(
           startAt: Duration(seconds: 5),
           showControls: true,
-          showFullscreenButton: true,
+          showFullscreenButton: false,
+          enableCaption: false,
           playsInline: true,
-          autoPlay: true),
+          autoPlay: false),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: YoutubePlayerControllerProvider(
-        // Provides controller to all the widget below it.
-        controller: _controller,
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {
+                  _controller.pause();
 
-        child: YoutubePlayerIFrame(
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            LandscopePlayVideo(videoid: widget.videoid),
+                      ));
+                },
+                icon: Icon(Icons.screen_rotation_outlined))
+          ],
+        ),
+        body: YoutubePlayerControllerProvider(
+          // Provides controller to all the widget below it.
           controller: _controller,
+
+          child: YoutubePlayerIFrame(
+            controller: _controller,
+          ),
         ),
       ),
     );
